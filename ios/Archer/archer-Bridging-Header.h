23d9ac1b82ba8a32ba3f0eccfcfe3a43bf44af66
Omar Mujahid <omar_mjhd@yahoo.com>
2015-09-25 23:37:46 -0400

    iOS Test code (We don't know how this actually works)

---
+++ ios/Archer/archer-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  archer-Bridging-Header.h
+//  Archer
+//
+//  Created by Omar Mujahid on 9/25/15.
+//  Copyright © 2015 Omar Mujahid. All rights reserved.
+//
+
+#ifndef archer_Bridging_Header_h
+#define archer_Bridging_Header_h
+
+
+#endif /* archer_Bridging_Header_h */
+#import <MyoKit/MyoKit.h>
